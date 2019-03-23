961009955086f0558c9e7d54fc9649adc8084f28
Terkwood <metaterkhorn@gmail.com>
2019-03-23 16:03:12 -0400

    stub out proc fence

--- ProcFence.gd
+++ ProcFence.gd
@@ -2,27 +2,28 @@ extends Node2D
 
 # from http://kidscancode.org/blog/2018/08/godot3_procgen1/
 var tile_size = 64  # tile size (in pixels)
-var vp = get_viewport()
-var width = 100
-var height = 50
+var offset_x = 10  # in tiles
+var offset_y = 5   # in tiles
+var width = 10     # in tiles
+var height = 1     # in tiles
 
 # get a reference to the map for convenience
 onready var Map = $TileMap
 
 func rand_tile_id():
-	return rand_range(4,7)
+	return rand_range(11,14)
 
-func make_maze():
+func make_fence():
 	Map.clear()
 	var valid_tiles = []
 	for x in range(width):
 		for y in range(height):
-			Map.set_cellv(Vector2(x,y), rand_tile_id())
+			Map.set_cellv(Vector2(x + offset_x,y + offset_y), rand_tile_id())
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	make_maze()
+	make_fence()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
