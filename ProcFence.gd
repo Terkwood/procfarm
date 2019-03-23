0fa93c0eba68a070e9f81205eab2e6e75c7dd835
Terkwood <metaterkhorn@gmail.com>
2019-03-23 16:23:55 -0400

    Improve ProcFence

--- ProcFence.gd
+++ ProcFence.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
 # from http://kidscancode.org/blog/2018/08/godot3_procgen1/
-var tile_size = 64  # tile size (in pixels)
-var offset_x = 10  # in tiles
-var offset_y = 5   # in tiles
-var width = 10     # in tiles
-var height = 1     # in tiles
+var max_offset_x = 24  # in tiles
+var max_offset_y = 18  # in tiles
+var min_width =  3     # in tiles
+var max_width = 10     # in tiles
+var height = 1         # in tiles
 
 # get a reference to the map for convenience
 onready var Map = $TileMap
@@ -16,6 +16,9 @@ func rand_tile_id():
 	return rand_range(11,14)
 
 func make_fence():
+	var width = min_width + randi()%max_width
+	var offset_x = randi()%max_offset_x
+	var offset_y = randi()%max_offset_y
 	Map.set_cellv(Vector2(offset_x, offset_y),
 					Set.find_tile_by_name("fence_alt_0"))
 	for x in range(width-2):
