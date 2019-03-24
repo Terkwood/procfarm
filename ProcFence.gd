e77962741ca62bcf8a1427183f8fd8fac3a50974
Terkwood <metaterkhorn@gmail.com>
2019-03-23 21:53:20 -0400

    Draw random fences
    Squashed commit of the following:
    
    commit 078db50f66d44ad11fc802bdbe5e8ed33426fbda
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Sat Mar 23 21:53:11 2019 -0400
    
        random fences
    
    commit a059b6b057eeaf93b6f00f578f120c384d1e416e
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Sat Mar 23 21:52:02 2019 -0400
    
        draw some fence
    
    commit d1f16d1c6590a6144df1a46344c0c0f4575b215d
    Author: Terkwood <metaterkhorn@gmail.com>
    Date:   Sat Mar 23 21:46:11 2019 -0400
    
        stub out fence

--- ProcFence.gd
+++ ProcFence.gd
@@ -1,28 +1,66 @@
 extends Node2D
 
-# from http://kidscancode.org/blog/2018/08/godot3_procgen1/
-var max_offset_x = 24  # in tiles
-var max_offset_y = 18  # in tiles
-var min_width =  3     # in tiles
-var max_width = 10     # in tiles
-var height = 1         # in tiles
+# adapted with inspiration from http://kidscancode.org/blog/2018/08/godot3_procgen1/
+var max_offset_x = 24   # in tiles
+var max_offset_y = 15   # in tiles
+var min_width  =  3     # in tiles
+var max_width  = 10     # in tiles
+var min_height =  3     # in tiles
+var max_height =  8     # in tiles
+
+const NW = 6
+const NE = 8
+const SW = 12
+const SE = 14
+const HORIZONTAL = 1
+const VERTICAL = 4
 
 # get a reference to the map for convenience
 onready var Map = $TileMap
 onready var Set = Map.tile_set
 
+func tile(c):
+	return Set.find_tile_by_name("fence_alt_"+str(c))
+
 func make_fence():
 	var width = min_width + randi()%max_width
+	var height = min_height + randi()%max_height
 	var offset_x = randi()%max_offset_x
 	var offset_y = randi()%max_offset_y	
-	Map.set_cellv(Vector2(offset_x, offset_y),
-					Set.find_tile_by_name("fence_alt_0"))
+	Map.set_cellv(
+		Vector2(offset_x, offset_y),
+		tile(NW)
+	)
+	Map.set_cellv(
+		Vector2(offset_x + width - 1, offset_y),
+		tile(NE))
+	Map.set_cellv(
+		Vector2(offset_x + width - 1, offset_y + height - 1),
+		tile(SE)
+	)
+	Map.set_cellv(
+		Vector2(offset_x, offset_y + height - 1),
+		tile(SW)
+	)
 	for x in range(width-2):
-		for y in range(height):
-			Map.set_cellv(Vector2(x + offset_x + 1, y + offset_y),
-				Set.find_tile_by_name("fence_alt_1"))	
-	Map.set_cellv(Vector2(offset_x + width - 1, offset_y),
-					Set.find_tile_by_name("fence_alt_2"))
+		Map.set_cellv(
+			Vector2(x + offset_x + 1, offset_y),
+			tile(HORIZONTAL))
+		Map.set_cellv(
+			Vector2(x + offset_x + 1, offset_y + height - 1),
+			tile(HORIZONTAL))
+	for y in range(height-2):
+		Map.set_cellv(
+			Vector2(
+				offset_x + width - 1,
+				y + offset_y + 1),
+			tile(VERTICAL))
+		Map.set_cellv(
+			Vector2(
+				offset_x ,
+				y + offset_y + 1),
+			tile(VERTICAL))
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
